9c8bf6a163a7ddd5f6a9204b72d1ec5d5ad391df
jincarry567 <1337151114@qq.com>
2021-05-29 22:52:32 +0800

    :pineapple: boot.asm

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -1,3 +1,8 @@
+;===========================================================
+; file-system:FAT12
+; loader-filename:LOADER.BIN
+;===========================================================
+
 org 0x7c00
 
 BaseOfStack equ 0x7c00
@@ -8,6 +13,8 @@ RootDirSectors equ 14
 SectorNumOfRootDirStart equ 19
 SectorNumOfFAT1Start equ 1
 SectorBalance equ 17
+
+;======== START FAT12 define
 
 jmp short Lablel_Start
 nop
@@ -30,6 +37,16 @@ BS_BootSig db 0x29
 BS_VolID dd 0
 BS_VolLab db 'boot loader'
 BS_FileSysType db 'FAT12   '
+
+;======== END FAT12 define
+
+;======== tmp var
+RootDirSizeForLoop dw RootDirSectors
+SectorNo dw 0
+Odd db 0
+StartBootMessage : db "Start Boot"
+NoLoaderMessage : db "ERROR:NO LOADER FOUND"
+LoaderFileName : db "LOADER BIN",0
 
 Lablel_Start:
     mov ax,cs
@@ -65,7 +82,169 @@ Lablel_Start:
     xor dl,dl
     int 13h
 
-StartBootMessage: db "Start Boot"
+;======= read one sector from floppy
+
+Func_ReadOneSector:
+    push bp
+    mov bp,sp
+    sub esp,2
+    mov byte [bp -2],cl
+    push bx
+    mov bl,[BPB_SecPerTrk]
+    div bl
+    inc ah
+    mov cl,ah
+    mov dh,al
+    shr al,1
+    mov ch,al
+    and dh,1
+    pop bx
+    mov dl,[BS_DrvNum]
+Label_Go_On_Reading:
+    mov ah,2
+    mov al,byte [bp -2]
+    int 13h
+    jc Label_Go_On_Reading
+    add esp,2
+    pop bp
+    ret
+
+；====== search loader.bin
+    mov word [SectorNo],SectorNumOfRootDirStart
+
+Label_Search_In_Root_Dir_Begin:
+    cmp word [RootDirSizeForLoop],0
+    jz Label_No_LoaderBin
+    dec word [RootDirSizeForLoop]
+    mov ax,00h
+    mov es,ax
+    mov bx,8000h
+    mov ax,[SectorNo]
+    mov cl,1
+    call Func_ReadOneSector
+    mov si,LoaderFileName
+    mov di,8000h
+    cld
+    mov dx,10h
+
+Label_Search_For_LoaderBin:
+    cmp dx,0
+    jz Label_Goto_Next_Sector_In_Root_Dir
+    dec dx
+    mov cx,11
+
+Label_Cmp_FileName:
+    cmp cx,0
+    jz Label_FileName_Found
+    dec cx
+    lodsb
+    cmp al,byte [es:di]
+    jz Label_Go_On
+    jmp Label_Different
+
+Label_Go_On:
+    inc di
+    jmp Label_Cmp_FileName
+
+Label_Different:
+    and di,0ffe0h
+    add di,20h
+    mov si,LoaderFileName
+    jmp Label_Search_For_LoaderBin
+    
+Label_Goto_Next_Sector_In_Root_Dir:
+    add word [SectorNo],1
+    jmp Label_Search_In_Root_Dir_Begin
+
+Label_No_LoaderBin:
+    mov ax,1301h
+    mov bx,008ch
+    mov dx,0100h
+    mov cx,21
+    push ax
+    mov ax,ds
+    mov es,ax
+    pop ax
+    mov bp,NoLoaderMessage
+    int 10h
+    jmp $
+
+Func_GetFATEntry:
+    push es
+    push bx
+    push ax
+    mov ax,00
+    mov es,ax
+    pop ax
+    mov byte [Odd],0
+    mov bx,3
+    mul bx
+    mov bx,2
+    div bx
+    cmp dx,0
+    jz Label_Even
+    mov byte [Odd],1
+
+Label_Even:
+    xor dx,dx
+    mov bx,[BPB_BytesPerSec]
+    div bx
+    push dx
+    mov bx,8000h
+    add ax,SectorNumOfFAT1Start
+    mov cl,2
+    call Func_ReadOneSector
+    pop dx
+    add bx,dx
+    mov ax,[es:bx]
+    cmp byte [Odd],1
+    jnz Label_Even_2
+    shr ax,4
+
+Label_Even_2:
+    and ax,0fffh
+    pop bx
+    pop es
+    ret
+
+Label_FileName_Found:
+    mov ax,RootDirSectors
+    and di,0ffe0h
+    add di,01ah
+    mov cx,word [es:di]
+    push cx
+    add cx,ax
+    add cx,SectorBalance
+    mov ax,BaseOfLoader
+    mov es,ax
+    mov bx,OffsetOfLoader
+    mov ax,cx
+
+Label_Go_On_Loading_File:
+    push ax
+    push bx
+    mov ah,0eh
+    mov al,'.'
+    mov bl,0fh
+    int 10h
+    pop bx
+    pop ax
+    mov cl,1
+    call Func_ReadOneSector
+    pop ax
+    call Func_GetFATEntry
+    cmp ax,0fffh
+    jz Label_File_Loaded
+    push ax
+    mov dx,RootDirSectors
+    add ax,dx
+    add ax,SectorBalance
+    add bx,[BPB_BytesPerSec]
+    jmp Label_Go_On_Loading_File
+
+Label_File_Loaded:
+    jmp $
+
     times 510 - ($ - $$) db 0
     dw 0xaa55
 
